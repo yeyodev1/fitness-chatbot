3616658b0a42591b1150675d30ef7b2645f3bcab
Diego Reyes <diegorele13@gmail.com>
2024-02-29 16:04:05 -0500

    remove unnecesary code

--- src/flows/voicenote.flow.ts
+++ src/flows/voicenote.flow.ts
@@ -2,11 +2,9 @@ import { EVENTS, addKeyword } from "@bot-whatsapp/bot";
 import { handlerAI } from "src/utils/voiceNoteHandler";
 
 const voicenoteFlow = addKeyword(EVENTS.VOICE_NOTE).addAction(
-  async (ctx, { state, flowDynamic, extensions, gotoFlow }) => {
+  async (ctx) => {
     try {
       await handlerAI(ctx);
-
-      flowDynamic("hola")
     } catch (err) {
       console.log(`[ERROR]:`, err);
       return;
